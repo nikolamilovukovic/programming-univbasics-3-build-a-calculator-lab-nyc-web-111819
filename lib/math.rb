ca1d0929bbf63e3989401b11f46e207ce5a86e0a
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-04 00:21:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,15 +1,17 @@
 def addition(num1, num2)
-  sum =(num2 + num1)
+  sum = (num2 + num1)
   sum
+  
 end
 
 def subtraction(num1, num2)
+  difference = (num2 - num1)
+  difference
   
-
 end
 
 def division(num1, num2)
-
+  
 end
 
 def multiplication(num1, num2)
